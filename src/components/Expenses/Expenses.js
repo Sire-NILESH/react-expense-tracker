3c49a7c2e2484877009485fdb6a5b9fccb062941
Nilesh Singh <singhnilesh9029@gmail.com>
2021-09-21 17:28:02 +0530

    final

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -25,7 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           getSelectedYearFilter={filterChangeHndler}
         />
-
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
